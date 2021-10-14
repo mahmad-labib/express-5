aeebe030d2a532c330ee9f7e20a74bb8cb9eb894
mahmad Labib <mohamed.anwarlabib@gmail.com>
2021-10-14 21:36:26 +0200

    upload files

--- api/adminController/articles.js
+++ api/adminController/articles.js
@@ -1,24 +1,38 @@
 const {
-    User,
-    Role,
-    Article,
-    Section
+  User,
+  Role,
+  Article,
+  Section
 } = require('../../mysql');
 
 var {
-    roles
+  roles
 } = require('../../conf/default')
 var admin = roles.admin;
 
-global.app.post('/admin/article', global.grantAccess(admin), async (req, res) => {
-    var {
-        title,
-        content,
-    } = req.body;
-    console.log(req.file);
-    // images.forEach(image=>{
-    //     console.log(image.basename) 
-    // })
-    // var query = Article.create({title, content})
-    res.json('s')
-})
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+
+var upload = multer({ storage: storage })
+
+global.app.post('/upload', global.grantAccess('admin'), upload.array('image', 10), (req, res) => {
+  var { uploadName } = req.body;
+  if (uploadName === 'article') {
+    articleUpload(req, res)
+  }
+  throw createError(404)
+});
+
+global.app.post('/admin/article', articleUpload);
+
+function articleUpload(req, res) {
+  res.send(req.files)
+}
