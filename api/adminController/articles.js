ca74c86ad02dcebc6ec4a27242856a2a55c4293c
mahmad Labib <mohamed.anwarlabib@gmail.com>
2021-10-17 00:06:06 +0200

    dd

--- api/adminController/articles.js
+++ api/adminController/articles.js
@@ -4,35 +4,63 @@ const {
   Article,
   Section
 } = require('../../mysql');
-
+var fs = require('fs');
 var {
   roles
 } = require('../../conf/default')
 var admin = roles.admin;
 
 var multer = require('multer');
+
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads');
+  destination: (req, file, cb) => {
+    const dest = `./uploads/${req.user.id}/${req.article.id}`
+    fs.access(dest, function (error) {
+      if (error) {
+        console.log("Directory does not exist.");
+        return fs.mkdir(dest, (error) => cb(error, dest));
+      } else {
+        console.log("Directory exists.");
+        return cb(null, dest);
+      }
+    });
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, Date.now() + file.originalname);
   }
 });
 
 
 var upload = multer({ storage: storage })
 
-global.app.post('/upload', global.grantAccess('admin'), upload.array('image', 10), (req, res) => {
+
+
+global.app.post('/upload', global.grantAccess(admin), async (req, res) => {
+  console.log(req.body)
   var { uploadName } = req.body;
-  if (uploadName === 'article') {
-    articleUpload(req, res)
+  if (uploadName == 'article') {
+    var article = await articleUpload;
+    req.article = article;
+    app.use(upload.array('image', 10))
+  } else {
+    throw createError(404)
   }
-  throw createError(404)
 });
 
 global.app.post('/admin/article', articleUpload);
 
-function articleUpload(req, res) {
-  res.send(req.files)
+
+async function articleUpload(data) {
+  var { content, title, section } = data;
+  var query = await Article.create({ title, content });
+  if (query) {
+    var attach_secions = await query.setSections(section);
+    if (!attach_secions) {
+      query.destroy();
+      throw createError(405)
+    }
+    return query;
+  } else {
+    throw createError(406)
+  }
 }
